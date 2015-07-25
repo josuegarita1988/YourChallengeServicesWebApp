c4a1539504170df047fde8e11fb42d20db1da7eb
Josue <josuegarita1988@gmail.com>
2015-07-25 16:33:44 -0600

    Limpiando archivos que no se usan

--- service/PlayerService.php
+++ service/PlayerService.php
@@ -155,5 +155,38 @@ class PlayerService extends Rest implements IPlayerService{
 		}
 		
 	}
+	
+	/*public function crearUsuario() {
+		if ($_SERVER['REQUEST_METHOD'] != "POST") {
+			$this->mostrarRespuesta($this->convertirJson($this->devolverError(1)), 405);
+		}
+		if (isset($this->datosPeticion['nombre'], $this->datosPeticion['email'], $this->datosPeticion['pwd'])) {
+			$nombre = $this->datosPeticion['nombre'];
+			$pwd = $this->datosPeticion['pwd'];
+			$email = $this->datosPeticion['email'];
+			if (!$this->existeUsuario($email)) {
+				$query = $this->getConnection()->prepare("INSERT into usuario (username,email,password) VALUES (:nombre, :email, :pwd)");
+				$query->bindValue(":nombre", $nombre);
+				$query->bindValue(":email", $email);
+				$query->bindValue(":pwd", sha1($pwd));
+				$query->execute();
+				if ($query->rowCount() == 1) {
+					$id = $this->getConnection()->lastInsertId();
+					$respuesta['estado'] = 'correcto';
+					$respuesta['msg'] = 'usuario creado correctamente';
+					$respuesta['usuario']['id'] = $id;
+					$respuesta['usuario']['nombre'] = $nombre;
+					$respuesta['usuario']['email'] = $email;
+					$this->mostrarRespuesta($this->convertirJson($respuesta), STATUS_OK);
+				}
+				else
+					$this->mostrarRespuesta($this->convertirJson($this->devolverError(7)), 400);
+			}
+			else
+				$this->mostrarRespuesta($this->convertirJson($this->devolverError(8)), 400);
+		} else {
+			$this->mostrarRespuesta($this->convertirJson($this->devolverError(7)), 400);
+		}
+	}*/
 
 }
