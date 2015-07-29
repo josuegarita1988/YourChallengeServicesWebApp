10c1e631df434bc7873c3043a8c5ec32bdb7a091
josuegarita1988 <josuegarita1988@gmail.com>
2015-07-29 15:57:26 -0600

    Update Api.php

--- Api.php
+++ Api.php
@@ -14,31 +14,6 @@ class Api {
 	private $arguments;
 	
 	public function __construct() {
-		$SERVER_DB = "OPENSHIFT_MYSQL_DB_HOST";
-	$SERVER_PORT = "OPENSHIFT_MYSQL_DB_PORT";
-	$USER_DB = "OPENSHIFT_MYSQL_DB_USERNAME";
-	$PASSWORD_DB = "OPENSHIFT_MYSQL_DB_PASSWORD";
-	$NAME_DB = "yourchallenge";
-	
-	 $connection = NULL;
-	
-	
-		$dsn = '';
-		$userName = '';
-		$password = '';
-	
-	
-			$dsn = 'mysql:dbname=' . $NAME_DB . ';host=' . getenv($SERVER_DB).':'.getenv($SERVER_PORT);
-			$userName = getenv($USER_DB);
-			$password = getenv($PASSWORD_DB);
-			
-	
-		try {
-			$this->connection = new \PDO($dsn, $userName, $password);
-			echo 'ok';
-		} catch (\PDOException $e) {
-			echo ($e->getMessage());
-		}
 	}
 	
 	private function getClass($className){
